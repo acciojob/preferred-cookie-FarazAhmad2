296487a11bd373e7e420afdf046ee4d7dad78f2a
Faraz Ahmad <102180103+FarazAhmad2@users.noreply.github.com>
2024-05-12 22:38:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ onLoad();
 
 const form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const fontSize = document.getElementById("fontsize").value;
-    const fontColor = document.getElementById("fontcolor").value;
-    document.cookie = `fontsize: ${fontSize}, fontcolor: ${fontColor}`;
+  e.preventDefault();
+  const fontSize = document.getElementById("fontsize").value;
+  const fontColor = document.getElementById("fontcolor").value;
+  document.cookie = `fontsize=${fontSize}; path=/`;
+  document.cookie = `fontcolor=${fontColor}; path=/`;
 });
 
 function onLoad() {
-    const data = new Map(document.cookie.split(', ').map(item => item.split(': ')));
-    document.getElementById("fontsize").value = data.get('fontsize');
-    document.getElementById("fontcolor").value = data.get('fontcolor');
+  const data = new Map(document.cookie.split("; ").map((item) => item.split("=")));
+  document.getElementById("fontsize").value = data.get("fontsize");
+  document.getElementById("fontcolor").value = data.get("fontcolor");
 }
